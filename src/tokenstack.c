59b6170551203db71628862fa64eb7bcf2cc1c33
Will Brown <kyrosewoodgang@gmail.com>
2022-01-25 09:56:23 -0500

    [tokenstack.c] Replace some switch-cases with lookup tables

--- src/tokenstack.c
+++ src/tokenstack.c
@@ -4,8 +4,14 @@
 
 #include "tokenstack.h"
 
-static_assert(NUM_INTRINSICS == 9, "Exhaustive handling of intrinsics in INTRINSIC_STR");
-static const char *INTRINSIC_STR[9] = {"abs", "sqrt", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan"};
+static_assert(NUM_OPERATORS == 5, "Exhaustive handling of operators in OPERATOR_CHAR_TABLE");
+static const char OPERATOR_CHAR_TABLE[5] = {'+', '-', '*', '/', '^'};
+
+static_assert(NUM_DELIMS == 2, "Exhaustive handling of delimiters in DELIM_CHAR_TABLE");
+static const char DELIM_CHAR_TABLE[2] = {'(', ')'};
+
+static_assert(NUM_INTRINSICS == 9, "Exhaustive handling of intrinsics in INTRINSIC_STR_TABLE");
+static const char *INTRINSIC_STR_TABLE[9] = {"abs", "sqrt", "ln", "sin", "cos", "tan", "arcsin", "arccos", "arctan"};
 
 TokenStack TokenStack_new() {
 	TokenStack stack = {
@@ -63,54 +69,12 @@ Token TokenStack_peek(const TokenStack *stack) {
 	return *stack->top;
 }
 
-static char operatorToChar(const Token *operator) {
-	static_assert(NUM_OPERATORS == 5, "Exhaustive handling of operators in operatorToChar");
-	if (operator->type != OPERATOR) {
-		fprintf(stderr, "Error translating operator to character: Provided token is not an operator.");
-		exit(2);
-	}
-
-	switch(operator->data.operator) {
-	case ADD:
-		return '+';
-	case SUB:
-		return '-';
-	case MUL:
-		return '*';
-	case DIV:
-		return '/';
-	case EXP:
-		return '^';
-	default:
-		fprintf(stderr, "Error translating operator to character: Invalid operator '%d'.\n", operator->data.operator);
-		exit(2);
-	}
-}
-
-static char delimToChar(const Token *delim) {
-	static_assert(NUM_DELIMS == 2, "Exhaustive handling of delimiters in delimToChar");
-	if (delim->type != DELIM) {
-		fprintf(stderr, "Error translating delimiter to character: Provided token is not a delimiter.");
-		exit(2);
-	}
-
-	switch(delim->data.delim) {
-	case OPEN_PAREN:
-		return '(';
-	case CLOSE_PAREN:
-		return ')';
-	default:
-		fprintf(stderr, "Error translating delimiter to character: Invalid delimiter '%d'.\n", delim->data.delim);
-		exit(2);
-	}
-}
-
 void TokenStack_print(const TokenStack *stack) {
 	static_assert(NUM_TYPES == 6, "Exhaustive handling of token types in TokenStack_print");
 	for (int i = 0; i < stack->length; i++) {
 		switch (stack->tokens[i].type) {
 		case OPERATOR:
-			printf("%c ", operatorToChar(&stack->tokens[i]));
+			printf("%c ", OPERATOR_CHAR_TABLE[stack->tokens[i].data.operator]);
 			break;
 		case OPERAND:
 			printf("%.15Lg ", stack->tokens[i].data.operand);
@@ -119,10 +83,10 @@ void TokenStack_print(const TokenStack *stack) {
 			printf("Error %d ", stack->tokens[i].data.err);
 			break;
 		case DELIM:
-			printf("%c ", delimToChar(&stack->tokens[i]));
+			printf("%c ", DELIM_CHAR_TABLE[stack->tokens[i].data.delim]);
 			break;
 		case INTRINSIC:
-			printf("%s ", INTRINSIC_STR[stack->tokens[i].data.intrinsic]);
+			printf("%s ", INTRINSIC_STR_TABLE[stack->tokens[i].data.intrinsic]);
 			break;
 		case NULL_TOKEN:
 			printf("Null ");
