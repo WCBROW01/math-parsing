516a4bc092579ab26ed95136accf2a11d75c8bb8
Will Brown <kyrosewoodgang@gmail.com>
2022-01-31 20:04:43 -0500

    [eval.c] Move evaluation functions into switch-case

--- src/eval.c
+++ src/eval.c
@@ -16,25 +16,6 @@ static Operand_t operator_div(Operand_t a, Operand_t b) {return a / b;}
 static_assert(NUM_OPERATORS == 6, "Exhaustive handling of operators in OPERATOR_FUNCS");
 static Operand_t (*OPERATOR_FUNCS[NUM_OPERATORS])(Operand_t, Operand_t) = {operator_add, operator_sub, operator_mul, operator_div, fmodl, powl};
 
-static void performOperation(TokenStack *evalStack) {
-	Token operator = TokenStack_pop(evalStack);
-	Token b 	   = TokenStack_pop(evalStack);
-	Token a		   = TokenStack_pop(evalStack);
-
-	Token result = {
-		.type = OPERAND,
-		.data.operand = OPERATOR_FUNCS[operator.data.operator](a.data.operand, b.data.operand)
-	};
-
-	TokenStack_push(evalStack, &result);
-}
-
-static void evaluateIntrinsic(TokenStack *evalStack) {
-
-	Token intrinsic = TokenStack_pop(evalStack);
-	INTRINSIC_FUNCS[intrinsic.data.intrinsic](evalStack);
-}
-
 Operand_t evaluateTokenStack(TokenStack *input) {
 	static_assert(NUM_TYPES == 6, "Exhaustive handling of token types in evaluateTokenStack");
 	if (input->length == 0) return NAN;
@@ -45,15 +26,20 @@ Operand_t evaluateTokenStack(TokenStack *input) {
 	for (int i = 0; i < input->length; i++) {
 		switch (input->tokens[i].type) {
 		case OPERATOR:
-			TokenStack_push(&evalStack, &input->tokens[i]);
-			performOperation(&evalStack);
+			Token b = TokenStack_pop(&evalStack);
+			Token a = TokenStack_pop(&evalStack);
+			Token result = {
+				.type = OPERAND,
+				.data.operand = OPERATOR_FUNCS[input->tokens[i].data.operator](a.data.operand, b.data.operand)
+			};
+
+			TokenStack_push(&evalStack, &result);
 			break;
 		case OPERAND:
 			TokenStack_push(&evalStack, &input->tokens[i]);
 			break;
 		case INTRINSIC:
-			TokenStack_push(&evalStack, &input->tokens[i]);
-			evaluateIntrinsic(&evalStack);
+			INTRINSIC_FUNCS[input->tokens[i].data.intrinsic](&evalStack);
 			break;
 		case DELIM:
 			// Do nothing, these are skipped in RPN.
