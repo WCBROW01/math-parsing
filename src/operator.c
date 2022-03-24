e78aa946d35f45b49039c130c69de0dfcc6f7e74
Will Brown <kyrosewoodgang@gmail.com>
2022-03-24 15:03:10 -0400

    Minor cleanup

--- src/operator.c
+++ src/operator.c
@@ -6,15 +6,15 @@
 #include "operator.h"
 
 static Token operator_assign(Token a, Token b) {
+	// Variable is an already initialized constant
 	if (a.data.var->flags & VAR_CONST && a.data.var->flags & VAR_INIT) {
 		fprintf(stderr, "Attempted to reassign a constant.\n");
 		return Token_throwError(6);
-	} else if (!(a.data.var->flags & VAR_INIT)) {
+	} else {
 		a.data.var->flags |= VAR_INIT;
+		a.data.var->data = b.data.operand;
+		return b;
 	}
-
-	a.data.var->data = b.data.operand;
-	return b;
 }
 
 static Token operator_add(Token a, Token b) {
